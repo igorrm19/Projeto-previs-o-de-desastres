fa238f04a584bcfdd4486a1bb72b7da89b95e444
igorrm19 <igorrodiguesmachado23@gmail.com>
2025-04-10 07:11:03 -0300

    feat: initialize back-end project with package.json and basic routing
    - Created package.json with essential dependencies and scripts for the back-end service.
    - Added routes.js to define a simple GET route that responds with "Hello wordl".

--- back-end/ap.js
+++ back-end/ap.js
@@ -1,9 +1,44 @@
-// Configuraçaõ principal
-// Ok
-// Ok teste
+import express from 'express';
+import cors from 'cors';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import Routes from './routes.js';
 
 
+const app = express();
+const port = 3000;
+Routes(app);
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '1.0.0',
+        info: {
+            title: 'back-end',
+            version: '1.0.0',
+            description: 'api back-end',
+        },
+        servers: [
+            {
+                url: `http://localhost:${port}`
+            }
+        ]
+    },
+    apis: ['./routes/*.js'] 
+}
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+app.use(cors(
+    {origin: '*'}
+))
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Hello wordl");
+   
+})
 
 
-
-
+app.listen(port, () => console.log("Servidor rodando na porta ", port));
