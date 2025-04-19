cf31ad4ff626d4bb841f3edfcda54e8d47ab2d3d
igorrm19 <igorrodiguesmachado23@gmail.com>
2025-04-19 18:36:13 -0300

    feat: adiciona configuração do banco de dados e define a tabela 'desastre' com índices

--- back-end/ap.js
+++ back-end/ap.js
@@ -5,6 +5,7 @@ import swaggerUi from 'swagger-ui-express';
 import Routes from './routes.js';
 import swaggerOptions from './swagger.js';
 import bodyParser from 'body-parser';
+import pgp from 'pg-promise';
 
 
 const app = express();
@@ -18,6 +19,12 @@ app.use(cors(
 ));
 
 app.use(bodyParser.json());
+
+const pgPromise = pgp();
+const db = pgPromise("postgres://igorrm19:chunda123@localhost:5432/desastre");
+db.query('SELECT 1 + 1 AS result').then((result) => console.log(result))
+
+console.log(db) 
 
 
 const user = [
