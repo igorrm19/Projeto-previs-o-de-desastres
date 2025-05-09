3d04146a593f67ef124abc7cea63e8fa0401c3db
igorrm10 <igorrodriguesmachado23@gmail.com>
2025-05-09 02:05:11 -0300

    refactor: ajusta estilo da página de erro e melhora a formatação do CSS global

--- src/features/error/pages/ErrorPage.jsx
+++ src/features/error/pages/ErrorPage.jsx
@@ -3,18 +3,23 @@ import useHooksNavigation from "../../../hooks/navegation";
 
 function ErrorPage() {
     const { goToLogin } = useHooksNavigation();
-  
-  
+
   return (
     <>
-      <main className="bg-fuchsia-700  min-h-screen flex items-center justify-center flex-col space-y-4">
+      <main className="bg-gray-900 text-white min-h-screen flex items-center justify-center flex-col space-y-4">
         <div className="">
           <SvdIlustration />
         </div>
+
         <div>
-          <h1 className="font-bold text-4xl space-y-4">404</h1>
+          <h1 className="font-bold text-4xl space-y-4 text-blue-500">404</h1>
+
           <p>Page Not Found</p>
-          <button onClick={goToLogin}  className="w-36 h-11 bg-gradient-to-r from-blue-800 via-orange-600 to-orange-700 text-gray-50 shadow-2xl rounded-xl hover:from-purple-800 hover:via-orange-600 hover:to-orange-700  transition-all duration-300">Back for home</button>
+
+           <button onClick={goToLogin} className="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-900 rounded-lg shrink-0 sm:w-auto hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
+                    Back to home
+          </button>
+
         </div>
       </main>
     </>
