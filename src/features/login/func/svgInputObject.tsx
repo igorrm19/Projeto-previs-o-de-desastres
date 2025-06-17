d65c6ba3a48bea54558177a7bf31265362dd307c
igorrm10 <igorrodriguesmachado23@gmail.com>
2025-06-16 21:46:29 -0300

    feat: adiciona suporte a visualização de senha e refatora componentes de botão e input; atualiza ícones SVG

--- src/features/login/func/svgInputObject.tsx
+++ src/features/login/func/svgInputObject.tsx
@@ -1,12 +1,14 @@
 import svgEmail from "../../../components/svg/svgEmail";
 import svgSenha from "../../../components/svg/svgSenha";
 import svgSenhaCadiado from "../../../components/svg/svgSenhaCadiado";
+import svgSenhaVisualizar from "../../../components/svg/svgSenhaVisualizar";
 
 
-const svgInputObject = {
-  email: svgEmail,
-  senha: svgSenha,
-  senhaCadiado: svgSenhaCadiado
-};
+const svgInputObject = [
+  { email: svgEmail },
+  { senha: svgSenha },
+  { senhaCadiado: svgSenhaCadiado },
+  { senhaVisualizar: svgSenhaVisualizar }
+] as const;
 
 export default svgInputObject;
