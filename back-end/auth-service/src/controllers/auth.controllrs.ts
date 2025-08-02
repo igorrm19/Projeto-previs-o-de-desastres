5f1af6ee2365b2a8e94fd0fc772b4b1869da8864
igorrm10 <igorrodriguesmachado23@gmail.com>
2025-08-01 21:25:12 -0300

    Initialize user-service with TypeScript setup, including package.json, tsconfig.json, and app.ts file.

---
+++ back-end/auth-service/src/controllers/auth.controllrs.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import validarLogin from "../services/auth.services";
+
+function login(req: Request, res: Response): void {
+  const { email, senha } = req.body;
+
+  const userFound: boolean = validarLogin(email, senha);
+
+  if (userFound) {
+    res.status(200).json({ message: "Login bem-sucedido" });
+  } else {
+    res.status(401).json({ message: "Credenciais inválidas" });
+  }
+}
+
+export default login;
---
+++ back-end/auth-service/src/models/user.models.ts
@@ -0,0 +1,13 @@
+
+ interface  User {
+ email: string;
+ senha: string;
+}
+
+
+const users: User[] = [
+  { email: 'igor@gmail.com', senha: '123' },
+  { email: 'jane@gmail.com', senha: '456' }
+];
+
+export default users;
